b60ec4a3c7ec771f6c4a4a7ddd4073691cdc7534
olejech <olejech@gmail.com>
2023-05-31 19:36:13 +0300

    feat(treesitter): ensure install vim language

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "javascript", "typescript", "tsx", "vimdoc", "lua", "markdown", "json", "graphql" },
+	ensure_installed = { "javascript", "typescript", "tsx", "vimdoc", "lua", "markdown", "json", "graphql", "vim" },
 	highlight = {
 		enable = true,
 	},
