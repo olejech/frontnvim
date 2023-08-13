7608152b60486ecd492b636855ddbd157d07cc9e
olejech <olejech@gmail.com>
2023-08-13 14:31:51 +0300

    feat(theme): use nightfox

--- lua/plugins/telekasten.lua
+++ lua/plugins/telekasten.lua
@@ -1,7 +1,7 @@
 local home = vim.fn.expand(os.getenv("ZETTELKASTEN_PATH"))
 require("telekasten").setup({
-	home = home .. "Base",
-	dailies = home .. "Daily",
-	templates = home .. "Templates",
-	template_new_note = home .. "Templates/NoteTelekastenTemplate.md",
+  home = home .. "Base",
+  dailies = home .. "Daily",
+  templates = home .. "Templates",
+  template_new_note = home .. "Templates/NoteTelekastenTemplate.md",
 })
