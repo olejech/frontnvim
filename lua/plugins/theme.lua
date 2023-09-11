bbd1310cc790fc90918969f383cfb7db0af57397
olejech <olejech@gmail.com>
2023-09-11 12:21:24 +0300

    fix(theme): override kanagawa theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,18 +1,24 @@
 require("kanagawa").setup({
-	colors = {
-		palette = {
-			peachRed = "#A3D4D5",
-			surimiOrange = "#E6C384",
-			samuraiRed = "#C34043",
-			waveRed = "#938AA9",
-		},
-		theme = {
-			all = {
-				ui = {
-					bg_gutter = "none",
-				},
-			},
-		},
-	},
+  overrides = function(colors) -- add/modify highlights
+    return {
+      Boolean = { link = "Special" },
+      ["@lsp.typemod.function.readonly"] = { bold = false },
+    }
+  end,
+  colors = {
+    palette = {
+      peachRed = "#A3D4D5",
+      surimiOrange = "#E6C384",
+      samuraiRed = "#C34043",
+      waveRed = "#938AA9",
+    },
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none",
+        },
+      },
+    },
+  },
 })
-vim.cmd.colorscheme("kanagawa")
+vim.cmd("colorscheme kanagawa")
