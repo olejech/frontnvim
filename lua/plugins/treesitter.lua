6bb530af369885fc39ac7803e986986e5e63e541
olejech <olejech@gmail.com>
2023-09-18 19:52:46 +0300

    fix(treesitter): enable indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,25 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		"javascript",
-		"typescript",
-		"tsx",
-		"vimdoc",
-		"lua",
-		"markdown",
-		"json",
-		"graphql",
-		"vim",
-		"html",
-		"css",
-		"scss",
-		"markdown_inline",
-	},
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = { "markdown" },
-	},
-	context_commentstring = {
-		enable = true,
-	},
+  ensure_installed = {
+    "javascript",
+    "typescript",
+    "tsx",
+    "vimdoc",
+    "lua",
+    "markdown",
+    "json",
+    "graphql",
+    "vim",
+    "html",
+    "css",
+    "scss",
+    "markdown_inline",
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = { "markdown" },
+  },
+  context_commentstring = {
+    enable = true,
+  },
+  indent = { enable = true },
 })
