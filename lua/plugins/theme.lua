765c333d2ff7254502ee708efab8cbeb0b690fb8
olejech <olejech@gmail.com>
2023-12-16 00:05:24 +0300

    feat(theme): change colors of Telescope

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,3 +1,13 @@
+vim.g.gruvbox_theme = {
+  TelescopeBorder        = { fg = '#ffffff', bg = '#000000' },
+  TelescopePreviewBorder = { fg = '#808080' },
+  TelescopeResultsBorder = { fg = '#808080' },
+  TelescopePromptBorder  = { fg = '#808080', bg = '#1F1F28' },
+  TelescopePromptTitle   = { fg = '#ffffff' },
+  TelescopePromptPrefix  = { bg = '#1F1F28' },
+  TelescopePromptNormal  = { fg = '#808080', bg = '#1F1F28' },
+}
+
 vim.g.gruvbox_flat_style = "hard"
 vim.g.gruvbox_italic_keywords = false
 vim.g.gruvbox_italic_functions = true
