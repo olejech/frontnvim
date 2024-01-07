00bcd718caaebe459828d9c4623c4caa74c1dd3b
olejech <olejech@gmail.com>
2024-01-07 17:35:14 +0300

    fix(format): support `telekasten` filetype

--- lua/plugins/format-on-save.lua
+++ lua/plugins/format-on-save.lua
@@ -2,23 +2,24 @@ local format_on_save = require("format-on-save")
 local formatters = require("format-on-save.formatters")
 
 format_on_save.setup({
-	exclude_path_patterns = {
-		"/node_modules/",
-		".local/share/nvim/lazy",
-	},
-	formatter_by_ft = {
-		html = formatters.lsp,
-		css = formatters.lsp,
-		scss = formatters.prettierd,
-		javascript = { formatters.prettierd, formatters.eslint_d_fix },
-		typescript = { formatters.prettierd, formatters.eslint_d_fix },
-		typescriptreact = { formatters.prettierd, formatters.eslint_d_fix },
-		json = formatters.prettierd,
-		lua = formatters.lsp,
-		markdown = formatters.prettierd,
-		yaml = formatters.lsp,
-	},
-	experiments = {
-		partial_update = "diff", -- or 'line-by-line'
-	},
+  exclude_path_patterns = {
+    "/node_modules/",
+    ".local/share/nvim/lazy",
+  },
+  formatter_by_ft = {
+    html = formatters.lsp,
+    css = formatters.lsp,
+    scss = formatters.prettierd,
+    javascript = { formatters.prettierd, formatters.eslint_d_fix },
+    typescript = { formatters.prettierd, formatters.eslint_d_fix },
+    typescriptreact = { formatters.prettierd, formatters.eslint_d_fix },
+    json = formatters.prettierd,
+    lua = formatters.lsp,
+    markdown = formatters.prettierd,
+    telekasten = formatters.prettierd,
+    yaml = formatters.lsp,
+  },
+  experiments = {
+    partial_update = "diff", -- or 'line-by-line'
+  },
 })
