fbe145c32b0f000b3d1f7e8d75bf1e504058581d
olejech <olejech@gmail.com>
2024-06-25 11:46:30 +0300

    fix(codeium): revert changes to commit

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -56,7 +56,9 @@ require("lazy").setup({
   { "RutaTang/quicknote.nvim",         event = "VeryLazy" },
   {
     "Exafunction/codeium.vim",
-    event = "BufEnter"
+    event = "BufEnter",
+    commit = "289eb724e5d6fab2263e94a1ad6e54afebefafb2"
+
   },
   { "ThePrimeagen/harpoon", event = "VeryLazy" },
   {
