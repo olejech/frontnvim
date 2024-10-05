7ce44c1aaf45a5b0f57e9f76d8cb6a404527c9d2
olejech <olejech@gmail.com>
2024-10-05 18:47:10 +0700

    chore(theme): update setup Nordic theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,6 @@
-local palette = require 'nordic.colors'
-
 require 'nordic'.setup({
   telescope = {
     style = 'classic',
-  },
-  override = {
-    NeoTreeGitModified = { fg = palette.orange.base },
-    NeoTreeGitUntracked = { fg = palette.green.base },
   },
 })
 
