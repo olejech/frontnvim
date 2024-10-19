70d3151b170bd4e792f2c76e8e7fd590473b136c
olejech <olejech@gmail.com>
2024-10-19 21:23:29 +0700

    fix(theme): update deprecated options

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,6 +2,10 @@ require 'nordic'.setup({
   telescope = {
     style = 'classic',
   },
+  on_highlight = function(highlights, palette)
+    highlights.NeoTreeGitModified = { fg = palette.orange.base }
+    highlights.NeoTreeGitUntracked = { fg = palette.green.base }
+  end,
 })
 
 vim.cmd.colorscheme 'nordic'
