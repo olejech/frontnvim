ede23cfc307b9e4829e67a714fcef8a2c68a4bf8
olejech <olejech@gmail.com>
2024-12-17 11:57:02 +0700

    feat(theme): change highlights

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,7 +5,12 @@ require 'nordic'.setup({
   on_highlight = function(highlights, palette)
     highlights.NeoTreeGitModified = { fg = palette.orange.base }
     highlights.NeoTreeGitUntracked = { fg = palette.green.base }
+    highlights.Visual = { bg = palette.gray2 }
   end,
+  on_palette = function(palette)
+    palette.blue2 = palette.orange.base
+    palette.cyan.base = palette.magenta.base
+  end
 })
 
 vim.cmd.colorscheme 'nordic'
