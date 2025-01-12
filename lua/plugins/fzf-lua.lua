2ee2284409bec17f859e60746dce5c26384fdc51
olejech <olejech@gmail.com>
2025-01-12 07:38:28 +0700

    feat(fzf-lua): add split mappings

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -8,8 +8,21 @@ require("fzf-lua").setup({
   },
   keymap = {
     builtin = {
+      true,
       ["<C-d>"] = "preview-page-down",
       ["<C-u>"] = "preview-page-up",
+    },
+    fzf = {
+      true,
+      ["ctrl-q"] = "select-all+accept",
+    },
+  },
+  actions = {
+    files = {
+      ["ctrl-x"] = actions.file_split,
+      ["ctrl-v"] = actions.file_vsplit,
+      ["ctrl-t"] = actions.file_tabedit,
+      ["ctrl-e"] = actions.file_sel_to_qf
     }
   },
   winopts = {
