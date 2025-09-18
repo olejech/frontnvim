bf83eb663e37ad514a22ae6ea81cc09b22f4e23c
olejech <olejech@gmail.com>
2025-09-18 21:18:36 +0300

    fix(langmapper): confict mappings

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -93,6 +93,7 @@ require("lazy").setup({
 	{
 		"Wansmer/langmapper.nvim",
 		lazy = false,
+		priority = 1, -- High priority is needed if you will use `autoremap()`
 	},
 	{
 		"folke/snacks.nvim",
