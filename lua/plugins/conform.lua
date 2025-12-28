36911431bf08d8b137733b2de7dfc45d51c4cf6a
olejech <olejech@gmail.com>
2025-12-28 19:18:06 +0300

    fix(lsp): lsp & fix format on save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,66 @@
+-- TODO: used in generic ui project
+local function select_prettier(bufnr, extra_formatters)
+	local has_prettier_json = vim.fs.find("prettier.json", {
+		upward = true,
+		path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":h"),
+	})[1]
+
+	local formatter = has_prettier_json and "prettier" or "prettierd"
+
+	if extra_formatters then
+		return vim.list_extend({ formatter }, extra_formatters)
+	end
+
+	return { formatter }
+end
+
 require("conform").setup({
 	formatters_by_ft = {
-		html = { "prettierd" },
-		css = { "prettierd", "stylelint" },
-		scss = { "prettierd", "stylelint" },
-		javascript = { "prettierd" },
-		javascriptreact = { "prettierd" },
-		typescript = { "prettierd" },
-		typescriptreact = { "prettierd" },
-		markdown = { "prettierd" },
-		mdx = { "prettierd" },
-		yaml = { "prettierd" },
-		astro = { "prettierd" },
+		html = function(bufnr)
+			return select_prettier(bufnr)
+		end,
+		css = function(bufnr)
+			return select_prettier(bufnr, { "stylelint" })
+		end,
+		scss = function(bufnr)
+			return select_prettier(bufnr, { "stylelint" })
+		end,
+		javascript = function(bufnr)
+			return select_prettier(bufnr)
+		end,
+		javascriptreact = function(bufnr)
+			return select_prettier(bufnr)
+		end,
+		typescript = function(bufnr)
+			return select_prettier(bufnr)
+		end,
+		typescriptreact = function(bufnr)
+			return select_prettier(bufnr)
+		end,
+		markdown = function(bufnr)
+			return select_prettier(bufnr)
+		end,
+		mdx = function(bufnr)
+			return select_prettier(bufnr)
+		end,
+		yaml = function(bufnr)
+			return select_prettier(bufnr)
+		end,
+		astro = function(bufnr)
+			return select_prettier(bufnr)
+		end,
 		lua = { "stylua" },
 	},
-	format_on_save = function(bufnr)
-		-- Disable with a global or buffer-local variable
-		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-			return
-		end
-		return { timeout_ms = 500, lsp_format = "fallback" }
-	end,
+	format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 	formatters = {
-		prettierd = {},
+		prettier = {
+			prepend_args = function(_, ctx)
+				local found = vim.fs.find("prettier.json", { upward = true, path = ctx.dirname })[1]
+				if found then
+					return { "--config", found }
+				end
+				return {}
+			end,
+		},
 	},
 })
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,7 +3,30 @@ local lua_ls_config = require("plugins.lsp.lua-ls")
 vim.lsp.config("lua_ls", lua_ls_config)
 vim.lsp.config("astro", {})
 vim.lsp.config("tailwindcss", {})
-vim.lsp.config("eslint", {})
+vim.lsp.config("eslint", {
+	on_attach = function(client, bufnr)
+		-- Auto-fix on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = bufnr,
+			callback = function()
+				local params = {
+					command = "eslint.applyAllFixes",
+					arguments = {
+						{
+							uri = vim.uri_from_bufnr(bufnr),
+							version = vim.lsp.util.buf_versions[bufnr],
+						},
+					},
+				}
+				-- Use request_sync to ensure fixes are applied before save
+				client.request_sync("workspace/executeCommand", params, 1000, bufnr)
+			end,
+		})
+	end,
+	settings = {
+		format = false,
+	},
+})
 vim.lsp.config("gopls", {})
 vim.lsp.config("stylelint_lsp", {})
 
